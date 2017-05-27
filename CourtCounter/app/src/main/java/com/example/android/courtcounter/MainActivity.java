db0710cc41dfa8525072ba15efde912b50485b85
Wojtek Opulski <kanidi93@gmail.com>
2017-05-28 00:02:13 +0200

    LinearLayout added, view split in half both sides. Team B with functionality added.

--- CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,26 +16,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void addTwoForTeamA(View view){
-        scoreTeamA = scoreTeamA+2;
+    public void addTwoForTeamA(View view) {
+        scoreTeamA = scoreTeamA + 2;
         displayForTeamA(scoreTeamA);
     }
 
-    public void addOneForTeamA(View view){
-        scoreTeamA = scoreTeamA+1;
+    public void addOneForTeamA(View view) {
+        scoreTeamA = scoreTeamA + 1;
         displayForTeamA(scoreTeamA);
     }
 
-    public void addThreeForTeamA(View view){
-        scoreTeamA = scoreTeamA+3;
+    public void addThreeForTeamA(View view) {
+        scoreTeamA = scoreTeamA + 3;
         displayForTeamA(scoreTeamA);
     }
 
-    public void resetValue(View v){
+    public void resetValue(View v) {
         scoreTeamA = 0;
         displayForTeamA(scoreTeamA);
     }
-
     /**
      * Displays the given score for Team A.
      */
@@ -42,4 +42,35 @@ public class MainActivity extends AppCompatActivity {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
+
+//  TEAM B METHODS
+
+    public void addTwoForTeamB(View view) {
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addOneForTeamB(View view) {
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addThreeForTeamB(View view) {
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void resetValueB(View v) {
+        scoreTeamB = 0;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
 }
