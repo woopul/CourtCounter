cc9f51d01f9ab695ff237c09e6567eb383d37d9a
Wojtek Opulski <kanidi93@gmail.com>
2017-05-28 14:38:42 +0200

    Styling app, set buttons style in style.xml . The line between two nested linea layouts added.

--- CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -7,7 +7,10 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Tracks the score of team A
     int scoreTeamA = 0;
+
+    // Tracks the score of team B
     int scoreTeamB = 0;
 
     @Override
@@ -16,21 +19,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /*
+    Increase the score of team A by 2
+     */
     public void addTwoForTeamA(View view) {
         scoreTeamA = scoreTeamA + 2;
         displayForTeamA(scoreTeamA);
     }
 
+    /**
+     * Inrease the score of team A by 1
+     */
     public void addOneForTeamA(View view) {
         scoreTeamA = scoreTeamA + 1;
         displayForTeamA(scoreTeamA);
     }
 
+    /**
+     * Inrease the score of team A by 3
+     */
     public void addThreeForTeamA(View view) {
         scoreTeamA = scoreTeamA + 3;
         displayForTeamA(scoreTeamA);
     }
 
+    /**
+     * reset score value of team A to 0
+     */
     public void resetValue(View v) {
         scoreTeamA = 0;
         displayForTeamA(scoreTeamA);
@@ -45,21 +60,33 @@ public class MainActivity extends AppCompatActivity {
 
 //  TEAM B METHODS
 
+    /**
+     * Increase score value of team B by 2
+     */
     public void addTwoForTeamB(View view) {
         scoreTeamB = scoreTeamB + 2;
         displayForTeamB(scoreTeamB);
     }
 
+    /**
+     * Increase score value of team B by 1
+     */
     public void addOneForTeamB(View view) {
         scoreTeamB = scoreTeamB + 1;
         displayForTeamB(scoreTeamB);
     }
 
+    /**
+     * Increase score value of team B by 3
+     */
     public void addThreeForTeamB(View view) {
         scoreTeamB = scoreTeamB + 3;
         displayForTeamB(scoreTeamB);
     }
 
+    /**
+     * reset score value of team B to 0
+     */
     public void resetValueB(View v) {
         scoreTeamB = 0;
         displayForTeamB(scoreTeamB);
@@ -71,6 +98,17 @@ public class MainActivity extends AppCompatActivity {
     public void displayForTeamB(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
+    }
+
+
+    /**
+     * Reset value both of the teams
+     */
+    public void resetBoth(View v){
+        scoreTeamA =0;
+        scoreTeamB =0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
     }
 
 }
